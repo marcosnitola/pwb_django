f6bf02f926b708eb58c897f70878b0cee27131ac
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-01 07:14:47 -0500

    Login config added

--- django_ex/django_ex/settings.py
+++ django_ex/django_ex/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#LOGIN CONFIGURATION
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

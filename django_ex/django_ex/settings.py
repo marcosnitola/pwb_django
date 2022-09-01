7978f92c0d873de8952d5c5ddfa81dd4878e9482
marcosraccoon <marcos123.mdnr@gmail.com>
2022-09-01 07:03:49 -0500

    Creating CRUD and templates

--- django_ex/django_ex/settings.py
+++ django_ex/django_ex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-_kpvo4xm!*gpms6=_59x1-=6ka*lhbju)gdh(myy0a=pxs(ekw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  '0.0.0.0',
+  '10.14.39.226',
+]
 
 
 # Application definition
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'django_ex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
